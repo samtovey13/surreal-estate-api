cd431b1b6d006d6848ae9ba1c4c296d6e66ef54c
Ian King <ian.antking@gmail.com>
2020-07-05 17:15:51 +0100

    change bedroom, bathroom and price properties to string

--- models/property.js
+++ models/property.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const PropertyListingSchema = new mongoose.Schema({
   title: { type: String, require: true },
   type: { type: String, require: true },
-  bedrooms: { type: Number, require: true },
-  bathrooms: { type: Number, require: true },
-  price: { type: Number, require: true },
+  bedrooms: { type: String, require: true },
+  bathrooms: { type: String, require: true },
+  price: { type: Sring, require: true },
   city: { type: String, require: true },
   email: { type: String, require: true },
 });
