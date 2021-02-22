42305a92c6d49f78be41129f66a79c90bb94b850
Sam Tovey <samtovey13@gmail.com>
2021-02-22 19:49:56 +0000

    added connection settings for MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const mongoose = require('mongoose');
+const mongoose,
+  { connect, connection } = require("mongoose");
 const restify = require('express-restify-mongoose');
 const app = express();
 const path = require('path');
@@ -23,7 +24,36 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 
 
-mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true });
+mongoose.connect(
+  process.env.DATABASE_CONN,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }).then(() => {
+    console.log('Connection established with MongoDB');
+})
+.catch(error => console.error(error.message));
+
+connection.on('connected', () => {
+  console.log('Mongoose connected to DB Cluster');
+})
+
+connection.on('error', (error) => {
+  console.error(error.message);
+})
+
+connection.on('disconnected', () => {
+  console.log('Mongoose Disconnected');
+})
+
+process.on('SIGINT', () => {
+  connection.close(() => {
+      console.log('Mongoose connection closed on Application Timeout');
+      process.exit(0);
+  })
+})
 
 restify.serve(router, PropertyListingModel);
 restify.serve(router, FavouriteModel);
